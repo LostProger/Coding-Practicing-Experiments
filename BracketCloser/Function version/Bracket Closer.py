b7aed36bcd0eb171a0cb8aa59878d80f943d28e7
LostProger <i5n3c9a3g7@gmail.com>
2021-03-31 14:35:19 +0300

    Better formatting, added samples

--- BracketCloser/Function version/Bracket Closer.py
+++ BracketCloser/Function version/Bracket Closer.py
@@ -1,52 +1,70 @@
-def pairdelete(t):
-    length = len(t)+1
-    while length > len(t):
-        length = len(t)
-        if t.find("()") > -1:
-            t = t.replace("()", "")
-        if t.find("[]") > -1:
-            t = t.replace("[]", "")
-        
-        if t.find("{}") > -1:
-            t = t.replace("{}", "")
-        
-    return t
+def pairdelete(line):
+    """Deletes pairs of brackets in line line until there are none left."""
+    length = len(line) + 1
+    while length > len(line):
+        length = len(line)
+        if line.find("()") > -1:
+            line = line.replace("()", "")
 
-def findcenter(a):
+        if line.find("[]") > -1:
+            line = line.replace("[]", "")
+
+        if line.find("{}") > -1:
+            line = line.replace("{}", "")
+
+    return line
+
+
+def findcenter(line):
+    """Finds potential highest-level brackets.
+    If there are 2 of them - returns their position and 2.
+    If there is only one such bracket - returns it's position and 1 (if it's a right bracket) or -1.
+    Otherwise returns -1 for both position and type."""
     for i in [")(", ")[", "){", "][", "](", "]{", "}{", "}(", "}["]:
-        pos = a.find(i)
+        pos = line.find(i)
         if pos > -1:
             return [pos, 2]
-    
-    searchr = max([a.rfind(")"), a.rfind("]"), a.rfind("}")])
+
+    searchr = max([line.rfind(")"), line.rfind("]"), line.rfind("}")])
     if searchr > -1:
         return [searchr, 1]
 
     found = []
-    searchl = -1
     for i in ["(", "[", "{"]:
-        if a.find(i) > -1:
-            found.append(a.find(i))
-        if found != []:
-            searchl = min[found]
-            return [searchl, -1]
+        if line.find(i) > -1:
+            found.append(line.find(i))
+        if found:
+            return [min[found], -1]
     return [-1, -1]
 
-def build(b):
-    swap = {"[":"]", "]":"[", "(":")", ")":"(", "{":"}", "}":"{"}
-    b = b[::-1]
+
+def build(line):
+    """Reverses brackets in a given line."""
+    swap = {"[": "]",
+            "]": "[",
+            "(": ")",
+            ")": "(",
+            "{": "}",
+            "}": "{"}
+    line = line[::-1]
     out = ''
-    for i in b:
+    for i in line:
         out += swap[i]
     return out
-    
-s = input()
-if s == "":
+
+
+inputstring = input()
+if inputstring == "":
     pass
-elif "(]" in s or "(}" in s or "[)" in s or "[}" in s or "{)" in s or "{]" in s:
+elif "(]" in inputstring \
+        or "(}" in inputstring \
+        or "[)" in inputstring \
+        or "[}" in inputstring \
+        or "{)" in inputstring \
+        or "{]" in inputstring:
     print("IMPOSSIBLE")
 else:
-    new = pairdelete(s)
+    new = pairdelete(inputstring)
     [split, sign] = findcenter(new)
     if split > -1:
         if sign == -1:
@@ -55,7 +73,25 @@ else:
         elif sign == 1:
             left = build(new)
             right = ""
-        else:
-            left = build(new[:split+1])
-            right = build(new[split+2:])
-        print(left+s+right)
+        else:                               # we need to complete input with 2 strings from each end
+            left = build(new[:split + 1])
+            right = build(new[split + 1:])
+        print(left + inputstring + right)
+
+"""
+Sample Input 1:
+
+}[[([{[]}
+
+Sample Output 1:
+
+{}[[([{[]}])]]
+
+Sample Input 2:
+
+{][[[[{}[]
+
+Sample Output 2:
+
+IMPOSSIBLE
+"""
